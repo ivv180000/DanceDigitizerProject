810ad9b9e8ee2c4fb061562211df6d4bbabe6ba7
ebrown18 <88006821+ebrown18@users.noreply.github.com>
2023-11-27 12:36:02 -0600

    front

--- DanceDigitizer/App.js
+++ DanceDigitizer/App.js
@@ -1,41 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView,Image,TouchableWithoutFeedback,TouchableOpacity,WebView,Dimensions} from 'react-native';
-import React, { useState, useEffect } from 'react';
-import VideoUploadComponent from "./components/VideoUploadComponent"
-import NotesBox from "./components/NotesBox"
-import VideoComponent from './components/VideoComponent';
+/*
+This file is the default screen of the project -  the root file
+that loads upon running "npx expo start" 
+*/
+import React from 'react';
+import{SafeAreaProvider} from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import SignUp from './screens/SignUp';
+import ForgotPassword from './screens/ForgotPassword';
+import Login from './screens/Login';
+import Content from './screens/Content';
+import Profile from './screens/Profile';
+import Upload from './screens/Upload';
+import VideoComponent from './screens/VideoComponent';
+import VideoUploadComponent from './screens/VideoUploadComponent';
+import NotesBox from './screens/NotesBox';
+import StartingTimeStampSelection from './screens/StartingTimeStampSelection';
+import Settings from './screens/Settings';
+import Favorites from './screens/Favorites';
+import Viewing from './screens/Viewing';
+import Record from './screens/Record';
+import VideoBreakdown from './screens/VideoBreakdown';
+import Loading from './screens/Loading';
 
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function App() {
-  
-  console.log("ho")
-  
+const ContentStack = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <VideoComponent/>
-      
-      
-      
-     
-      {
-
-      }
-    </SafeAreaView>
+    <Stack.Navigator>
+      <Stack.Screen name="Content" component={Content} />
+      <Stack.Screen name="VideoStack" component={VideoStack} />
+    </Stack.Navigator>
   );
+};
+
+const UploadStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Upload" component={VideoUploadComponent} />
+      //<Stack.Screen name = "Record" component={Record} />
+    </Stack.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#purple',
-  },
-  notesBoxContainer: {
-    justifyContent: 'center', alignItems: 'center' ,
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 10,
-    padding: 10,
-  
-  },
-});
+const VideoStack = () => {
+  return (
+    <Stack.Navigator>
+      //<Stack.Screen name="Viewing" component={Viewing} />
+      //<Stack.Screen name="Loading" component={Loading} />
+      //<Stack.Screen name = "VideoBreakdown" component={VideoBreakdown} />
+      <Stack.Screen name = "VideoBreakdown" component= {VideoComponent} />
+    </Stack.Navigator>
+  )
+}
+
+const AppTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="ContentStack" component={ContentStack} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="UploadStack" component={UploadStack} />
+      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+    </Tab.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <SafeAreaProvider> 
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="ContentStack" component={ContentStack} />
+          <Stack.Screen name="AppTabs" component={AppTabs} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
