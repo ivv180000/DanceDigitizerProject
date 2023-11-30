a667ab58ece8fa32f46ac7b875c2318d666c8cc2
ivv180000 <149630771+ivv180000@users.noreply.github.com>
2023-11-29 20:58:49 -0600

    Update Profile.js to include the basic Profile frame/screen

--- DanceDigitizer/screens/Profile.js
+++ DanceDigitizer/screens/Profile.js
@@ -1,22 +1,102 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, Button, TouchableOpacity, Image} from 'react-native';
 import {PageHeader} from './components/PageHeader';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {BottomButtons} from './components/BottomButtons';
+import {ButtonPrimary} from './components/ButtonPrimary';
 
 export function Profile(){
-    const insets = useSafeAreaInsets();
-    return (
-        <View style={[styles.root, { paddingTop: insets.top }]}>
-          <PageHeader headerText="Profile" />
+
+  return(
+    <View>
+      <View style={styles.top}>
+        <View style={styles.TopButtons}>
+          <TouchableOpacity>
+            <Text style={styles.TopButtonText}> Settings </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Text style={styles.TopButtonText}> Logout </Text>
+          </TouchableOpacity>
         </View>
-      );
-    }
-    
-    const styles = StyleSheet.create({
-      root: {
-        width: 375,
-        height: 812,
-        flexShrink: 0,
-        flex: 1, 
-      },
-    });
+
+        <Text style={styles.HeaderText}> Profile </Text>
+      </View>
+      <View style={styles.middle}>
+        <View>
+          <View style={styles.ProfileImage}>
+            <Image style={styles.image} source={require('../assets/Profile_Button.png')}/>
+          </View>
+
+          <View>
+            <Text style={styles.NameText}> NAME </Text>
+            <Text style={styles.DescriptionText}> description </Text>
+          </View>
+
+        </View>
+        <ScrollView style={styles.content}>
+          <Text> CONTENT GOES HERE </Text>
+        </ScrollView>
+      </View>
+      <View style={styles.bottom}>
+        <BottomButtons/>
+      </View>
+    </View>
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  top: {
+    height: '15%',
+    justifyContent: 'center',
+    backgroundColor: '#5DB075'
+  },
+  TopButtons: {
+    flexDirection: 'row',
+    marginTop: 40,
+    justifyContent: 'space-between'
+  },
+  TopButtonText: {
+    color: '#FFFFFF'
+  },
+  HeaderText: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  bottom: {
+    height: '15%',
+    backgroundColor: '#e8e8e8'
+  },
+  image:{
+    width: 60,
+	  height: 60,
+    borderRadius: 70,
+    overflow: "hidden",
+    borderColor: '#F5F5F5',
+  },
+  middle: {
+    height: '70%',
+    padding: 20,
+    justifyContent: 'space-evenly',
+    borderColor: '#FFFFFF'
+  },
+  NameText:{
+    textAlign: 'center',
+    fontSize: 30
+  },
+  DescriptionText: {
+    textAlign: 'center',
+    fontSize: 20
+  },
+  content: {
+    backgroundColor: '#F5F5F5',
+    height: '50%'
+  },
+  ProfileImage: {
+    alignItems: 'center'
+  }
+});
+
 export default Profile;
